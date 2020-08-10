28c101afdae2f045ee34e68c5ea319326614ed05
PizzaCrust <pizzacrustcoder@gmail.com>
2020-08-10 23:03:35 +0700

    change how we measure time

--- src/main.rs
+++ src/main.rs
@@ -31,30 +31,28 @@ error_chain! {
     }
 }
 
-macro_rules! measure {
-    ($expr:expr;) => {{
-           let start = SystemTime::now();
-           let value = $expr;
-           println!("took {} ms", SystemTime::now().duration_since(start)?.as_millis());
-           value
-    }};
+fn measure(block: fn() -> Result<()>) -> Result<()> {
+    let current_time = SystemTime::now();
+    block();
+    println!("took {} ms", SystemTime::now().duration_since(current_time)?.as_millis());
+    Ok(())
 }
 
 fn main() -> Result<()> {
-    let replay = measure! {
-        UReplay::parse(std::fs::read("season12.replay")?);
-    }.map_err(|e| crate::Error::with_chain(e, crate::ErrorKind::BincodeError))?;
-    for x in replay.chunks {
-        if x.variant == 0 {
-            //println!("{:?}", bincode::deserialize::<HeaderChunk>(x.data.as_slice()))
-            println!("{:?}", HeaderChunk::parse(x));
-        } else if x.variant == 3 {
-            let e_chunk = EventChunk::parse(x, replay.meta.encryption_key.as_slice())?;
-            if e_chunk.group == "playerElim" {
-                println!("{:?}", Elimination::parse(e_chunk))
+    measure(|| {
+        let replay= UReplay::parse(std::fs::read("season12.replay")?)?;
+        for x in replay.chunks {
+            if x.variant == 0 {
+                //println!("{:?}", bincode::deserialize::<HeaderChunk>(x.data.as_slice()))
+                println!("{:?}", HeaderChunk::parse(x));
+            } else if x.variant == 3 {
+                let e_chunk = EventChunk::parse(x, replay.meta.encryption_key.as_slice())?;
+                if e_chunk.group == "playerElim" {
+                    println!("{:?}", Elimination::parse(e_chunk))
+                }
             }
         }
-    }
-    //println!("{:?}", header_chunk);
+        Ok(())
+    });
     Ok(())
 }
